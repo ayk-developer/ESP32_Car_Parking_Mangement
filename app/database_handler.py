ba2f7e3d38dae482c432fdeefd91873d8ab71c4f
Aung Ye Kyaw <agyekyaw007@gmail.com>
2019-08-05 01:46:48 +0630

    Multi car parking support added

--- app/database_handler.py
+++ app/database_handler.py
@@ -1,14 +1,18 @@
 import psycopg2
+from psycopg2.extensions import AsIs
+from psycopg2 import sql
 class Database():
-    def __init__(self):
-        self.conn=psycopg2.connect("dbname='d444nf8njcg92a' user='atwzycuaabdruo' password='91fedb3eee2d763d798890cf015a50f4254b41e84c065cb631eb8d84742c5c40' host='ec2-174-129-41-127.compute-1.amazonaws.com' port='5432'")
+    def __init__(self,parking):
+        self.parking=parking
+        self.conn=psycopg2.connect("dbname='car_parking' user='postgres' password='11111111' host='localhost' port='5432'")
         self.cur=self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS parking1(id serial PRIMARY KEY,rfid varchar(20),time abstime NOT NULL DEFAULT CURRENT_TIMESTAMP,inout varchar(5))")
+
+        self.cur.execute("CREATE TABLE IF NOT EXISTS {} (id serial PRIMARY KEY,rfid varchar(20),time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,inout varchar(5))".format(self.parking))
         #self.cur.execute("CREATE TABLE IF NOT EXISTS user(id serial PRIMARY KEY,username varchar(64),email varchar(120),password_hash varchar(128))")
         self.cur.execute("CREATE TABLE IF NOT EXISTS billing(rfid varchar(20) PRIMARY KEY, current_amount INTEGER)")
         self.cur.execute("CREATE TABLE IF NOT EXISTS rfidnumber(rfid varchar(20) PRIMARY KEY, rfidtag varchar(20))")
         #self.cur.execute("CREATE TABLE IF NOT EXISTS accounts(username STRING PRIMARY KEY, password string)")
-        #self.conn.commit()
+        self.conn.commit()
         #self.current_cars=[]
 
     def tagtonumber(self,tag):
@@ -28,7 +32,7 @@ class Database():
 
     def carinout(self,rfid_number,inout):
         rfid_number=self.tagtonumber(rfid_number)
-        self.cur.execute("INSERT INTO parking1(rfid,inout) VALUES(%s,%s)",[rfid_number,inout])
+        self.cur.execute("INSERT INTO {}(rfid,inout) VALUES(%s,%s)".format(self.parking),[rfid_number,inout])
         self.conn.commit()
         #if inout=="in":
         #    self.current_cars.append(rfid_number)
@@ -37,21 +41,48 @@ class Database():
 
 
 
-    def view(self):
-        self.cur.execute("SELECT id,rfid,time,inout FROM parking1")
+    def viewadmin(self):
+        a=parkinglist()
+        temp_dict={}
+        for i in a:
+            self.cur.execute("SELECT id,rfid,time,inout FROM {}".format(i))
+            row=self.cur.fetchall()
+            temp_dict.update({i:row})
+        return temp_dict
+        
+
+    def view(self,rfid):
+        a=parkinglist()
+        temp_dict={}
+        for i in a:
+            self.cur.execute("SELECT time,inout FROM {} where rfid =%s".format(i),(rfid,))
+            row=self.cur.fetchall()
+            temp_dict.update({i:row})
+        return temp_dict
+
+    def viewbill(self,rfid):
+        self.cur.execute("SELECT * FROM billing where rfid=%s",(rfid,))
         row=self.cur.fetchall()
         return row
 
-    def viewbill(self):
+    def viwebilladmin(self):
         self.cur.execute("SELECT * FROM billing")
         row=self.cur.fetchall()
         return row
 
     def cutbill(self,rfid):
+        self.cur.execute("select last_value(time) over() from {} where inout='out' limit 1".format(self.parking))
+        outtime=self.cur.fetchall()[0][0]
+        self.cur.execute("select last_value(time) over() from {} where inout='in' limit 1".format(self.parking))
+        intime=self.cur.fetchall()[0][0]
+        dur=outtime-intime
+        cost=int(dur.seconds*0.083)
+        print(cost)
+        rfid=self.tagtonumber(rfid)
         self.cur.execute("SELECT current_amount FROM billing WHERE rfid=%s",(rfid,))
         current_amount=self.cur.fetchall()
-        #print(type(current_amount[0][0]))
-        current_amount=current_amount[0][0]-50
+        print(current_amount[0][0])
+        current_amount=current_amount[0][0]-cost
         self.cur.execute("UPDATE billing SET current_amount=%s WHERE rfid=%s",(current_amount,rfid))
         self.conn.commit()
 
@@ -71,19 +102,50 @@ class Database():
         self.conn.commit()
 
     def search(self,rfid):
-        self.cur.execute("SELECT * FROM parking1 WHERE rfid=%s",(rfid,))
+        self.cur.execute("SELECT * FROM {} WHERE rfid=%s".format(self.parking),(rfid,))
         row=self.cur.fetchall()
         return row
 
     def carcount(self):
-        self.cur.execute("SELECT COUNT(*) FROM parking1 Where inout = 'in'")
+        self.cur.execute("SELECT COUNT(*) FROM {} Where inout = 'in'".format(self.parking))
         incar=self.cur.fetchall()[0][0]
-        self.cur.execute("SELECT COUNT(*) FROM parking1 Where inout = 'out'")
+        self.cur.execute("SELECT COUNT(*) FROM {} Where inout = 'out'".format(self.parking))
         outcar=self.cur.fetchall()[0][0]
         amount=incar-outcar
         return amount
 
 
+def parkinglist():
+    conn=psycopg2.connect("dbname='car_parking' user='postgres' password='11111111' host='localhost' port='5432'")
+    cur=conn.cursor()
+
+    cur.execute("SELECT tablename FROM pg_catalog.pg_tables where schemaname='public'")
+
+    a=[item[0] for item in cur.fetchall()]
+
+    a.remove('billing')
+    a.remove('rfidnumber')
+    return a
+
+def occupied():
+    conn=psycopg2.connect("dbname='car_parking' user='postgres' password='11111111' host='localhost' port='5432'")
+    cur=conn.cursor()
+    a=parkinglist()
+    temp_dict={}
+    for i in a:
+        cur.execute("SELECT COUNT(*) FROM {} Where inout = 'in'".format(i))
+        incar=cur.fetchall()[0][0]
+        cur.execute("SELECT COUNT(*) FROM {} Where inout = 'out'".format(i))
+        outcar=cur.fetchall()[0][0]
+        amount=incar-outcar
+        temp_dict.update({i:amount})
+    return temp_dict
+
+        
+
+
+
+#database=Database('lol')
 #database.carinout("AA0065","in")
 #database.carinout("AC0098","in")
 #database.carinout("AB0065","in")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
 from app import carcounter
 from app import app
 from app import db
-from flask import render_template,request,flash,redirect,url_for
+from flask import render_template,request,flash,redirect,url_for,Response
 from app.forms import LoginForm,RegistrationForm
 from flask_login import current_user, login_user,logout_user,login_required
 from app.models import User
-from app.database_handler  import Database
+from app.database_handler  import Database,occupied
 from tabulate import tabulate
 
 
@@ -13,9 +13,7 @@ from tabulate import tabulate
 @app.route('/')
 @app.route("/index")
 def index():
-    database=Database()
-    carcount=database.carcount()
-    print(carcount)
+    carcount=occupied()
     return render_template("homepage.html",carcount=carcount)
 
 @app.route('/about/')
@@ -62,30 +60,43 @@ def logout():
 @app.route('/parkingdata/')
 @login_required
 def parkingdata():
-    #if (request.form["username"] == "Aung Ye Kyaw") & (request.form["password"] == "11111111"):
-        database=Database()
-        table = tabulate(database.view(), tablefmt='html')
+    database=Database('parking1')
+    if current_user.username=='Admin':
+        table = tabulate(database.viewadmin(), tablefmt='html')
         return render_template("parkingdata.html",table=table)
+    else:
+        table = tabulate(database.view(current_user.username), tablefmt='html')
+        return render_template("parkingdata.html",table=table)
+
+
 
 @app.route('/billingdata/')
 @login_required
 def billingdata():
-        database=Database()
-        table = tabulate(database.viewbill(), tablefmt='html')
+    database=Database('parking1')
+    if current_user.username=='Admin':
+        table = tabulate(database.viwebilladmin(), tablefmt='html')
+        return render_template("parkingdata.html",table=table)
+    else:
+        table = tabulate(database.viewbill(current_user.username), tablefmt='html')
         return render_template("parkingdata.html",table=table)
 
 @app.route('/searchdata/',methods=['POST'])
 @login_required
 def searchdata():
-        #a=Database()
-    if request.method == 'POST':
-        data= (request.form["rfid"])
-        database=Database()
-        table = tabulate(database.search(data), tablefmt='html')
-        return render_template("parkingdata.html",table=table)
-        #table = tabulate(request.form["rfid"].search(), tablefmt='html')
-        #return render_template("parkingdata.html",table=table)
-    return "aaa"
+    if current_user.username=='Admin':
+        #a=Database('parking1')
+        if request.method == 'POST':
+            data= (request.form["rfid"])
+            database=Database('parking1')
+            table = tabulate(database.search(data), tablefmt='html')
+            return render_template("parkingdata.html",table=table)
+            #table = tabulate(request.form["rfid"].search(), tablefmt='html')
+            #return render_template("parkingdata.html",table=table)
+        return "aaa"
+    else:
+        return "<h1>You Dont Have Permission to access Here</h1>"
+    
 
 @app.route('/getdata/',methods=['GET','POST'])
 def getdata():
@@ -93,23 +104,29 @@ def getdata():
         rfid = request.form.get('rfid')
         inout= request.form.get('inout')
         pwd=request.form.get('pwd')
+        a=request.form.get('parking')
         if pwd == 'lolhaha':
 
-            database=Database()
+            database=Database(a)
             database.carinout(rfid,inout)
+            if inout == 'out':
+                database.cutbill(rfid)
+            '''
             if inout == 'in':
                 carcounter.carin(rfid)
             elif inout == 'out':
                 carcounter.carout(rfid)
             else:
                 pass
+            '''
         else:
             return '''<h1>BUUUUUUUU BUU DESU WA</h1><img src="https://vignette.wikia.nocookie.net/love-live/images/2/27/LLSS_S1Ep1_250.png/revision/latest/scale-to-width-down/800?cb=20160703203036" alt="BUUUUUUUU BUUU DESU WA">'''
 
     return '''<form method="POST">
                   rfid: <input type="text" name="rfid"><br>
                   inout: <input type="text" name="inout"><br>
-                  pwd: <<input type="text" name="pwd"><br>
+                  pwd: <input type="text" name="pwd"><br>
+                  parking : <input type="text" name="parking"><br>
                   <input type="submit" value="Submit"><br>
               </form>
               '''
@@ -117,13 +134,16 @@ def getdata():
 @app.route('/addbill/', methods=['GET','POST'])
 @login_required
 def addbill():
-    if request.method == 'POST':
-        rfid = request.form.get('rfid')
-        amount= request.form.get('amount')
-        tag=request.form.get('tag')
-        database=Database()
-        database.addbill(rfid,tag,int(amount))
-        table = tabulate(database.viewbill(), tablefmt='html')
-        return render_template("parkingdata.html",table=table)
+    if current_user.username=='Admin':
+        if request.method == 'POST':
+            rfid = request.form.get('rfid')
+            amount= request.form.get('amount')
+            tag=request.form.get('tag')
+            database=Database('parking1')
+            database.addbill(rfid,tag,int(amount))
+            table = tabulate(database.viwebilladmin(), tablefmt='html')
+            return render_template("parkingdata.html",table=table)
+        else:
+            return render_template('addbill.html')
     else:
-        return render_template('addbill.html')
+        return "<h1>You Dont Have Permission to access Here</h1>"
